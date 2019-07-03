bbb64846851ced4146666c7206a1022e7d2d23aa
Bob Afwata <bobraphtoneafwata@gmail.com>
2019-07-03 22:19:52 +0300

    docs

--- src/main.py
+++ src/main.py
@@ -23,71 +23,7 @@ SENSOR5_ECHO = 6                                  #Associate pin 14 to Echo
 
 trig_pins = {SENSOR1_TRIG,SENSOR2_TRIG,SENSOR3_TRIG,SENSOR4_TRIG,SENSOR5_TRIG}
 echo_pins = {SENSOR1_ECHO,SENSOR2_ECHO,SENSOR3_ECHO,SENSOR4_ECHO,SENSOR5_ECHO}
-
-def init_sensors():
-    trig_pins = {SENSOR1_TRIG,SENSOR2_TRIG,SENSOR3_TRIG,SENSOR4_TRIG,SENSOR5_TRIG}
-    echo_pins = {SENSOR1_ECHO,SENSOR2_ECHO,SENSOR3_ECHO,SENSOR4_ECHO,SENSOR5_ECHO}
-    #set pins as output and input
-
-    for i in trig_pins:
-        GPIO.setup(i,GPIO.OUT)
-        time.sleep(0.5)
-    for i in echo_pins:
-        GPIO.setup(i,GPIO.IN)                   #Set pin as GPIO in
-    print('sensors successfully initialized....')
-
-init_sensors()
-
-def pull_trig_low(trig_pins):
-    trig_pins = {SENSOR1_TRIG,SENSOR2_TRIG,SENSOR3_TRIG,SENSOR4_TRIG,SENSOR5_TRIG}
-    for i in trig_pins:
-            GPIO.output(i, False)                 #Set TRIG as LOW
-
-def pull_trig_high(trig_pins):
-    trig_pins = {SENSOR1_TRIG,SENSOR2_TRIG,SENSOR3_TRIG,SENSOR4_TRIG,SENSOR5_TRIG}
-    for i in trig_pins:
-            GPIO.output(i, True)                 #Set TRIG as LOW
-
-
-
-def pull_echo_low(echo_pins):
-    echo_pins = {SENSOR1_ECHO,SENSOR2_ECHO,SENSOR3_ECHO,SENSOR4_ECHO,SENSOR5_ECHO}
-    for i in echo_pins:
-            GPIO.output(i, False)                 #Set TRIG as LOW
-
-def pull_echo_high(echo_pins):
-    echo_pins = {SENSOR1_ECHO,SENSOR2_ECHO,SENSOR3_ECHO,SENSOR4_ECHO,SENSOR5_ECHO}
-    for i in echo_pins:
-            GPIO.output(i, True)                 #Set TRIG as LOW
-
-
-
-while True:
-    pull_trig_low(trig_pins)
-    #GPIO.output(SENSOR1_TRIG, False)                 #Set TRIG as LOW
-    print("Waiting For Sensors To Settle")
-    time.sleep(2)  
-                              #Delay of 2 seconds
-    pull_trig_high(trig_pins)
-    #GPIO.output(SENSOR1_TRIG, True)                  #Set TRIG as HIGH
-    time.sleep(0.00001)                      #Delay of 0.00001 seconds
-    pull_trig_low(trig_pins)
-    #GPIO.output(SENSOR1_TRIG, False)                 #Set TRIG as LOW
-
-    while GPIO.input(SENSOR1_ECHO)==0:
-        pulse_start = time.time()              #Time of the last  LOW pulse
-
-    while GPIO.input(SENSOR1_ECHO)==1:               #Check whether Echo is HIGH
-        pulse_end = time.time()                #Time of the last HIGH pulse 
-        pulse_duration = pulse_end - pulse_start #pulse duration to a variable
-        distance = pulse_duration * 17150        #Calculate distance
-        distance = round(distance, 2)            #Round to two decimal points
-    if distance > 20 and distance < 400:     #Is distance within range
-        print("Distance:",distance - 0.5,"cm" ) #Distance with calibration
-        # send it to api 
-    else:
-        print("Out Of Range")                  #display out of range
-
+GPIO.setwarnings(False)
 
 url = 'http://ultrapisensor.caramacs.com/api/sensor_data'
 
@@ -95,14 +31,97 @@ def get_data(url):
     r = requests.get(url)
     print(r.text)
 
+distance = 0
 
+payload = {'sensor_id': '044ea4f4-fd82-482c-97d1-7c190c70cbd7', 'value': distance}
 
-sensor_data = {'sensor_id' :'84cd5606-9cfb-11e9-a2a3-2a2ae2dbcce4' ,'value ': 0 }
+def send_sensor_data(url,payload):
+    r = requests.post(url, data=payload)
 
-def send_sensor_data(url,sensor_data):
-    r = requests.post(url, data = {'name':'device1', 'device_uid':'123e4567-e89b-12d3-a456-426655440000'})
-    time.sleep(5)
+    print(r.status_code)
     #end of sending data
+
+
+def init_sensors(trig_pins,echo_pins):
+    #trig_pins = {SENSOR1_TRIG,SENSOR2_TRIG,SENSOR3_TRIG,SENSOR4_TRIG,SENSOR5_TRIG}
+    #echo_pins = {SENSOR1_ECHO,SENSOR2_ECHO,SENSOR3_ECHO,SENSOR4_ECHO,SENSOR5_ECHO}
+    #set pins as output and input
+
+    for i in trig_pins:
+        GPIO.setup(i,GPIO.OUT)
+        time.sleep(0.5)
+    for i in echo_pins:
+        GPIO.setup(i,GPIO.IN)                   #Set pin as GPIO in
+    
+    print('sensors successfully initialized....')
+
+init_sensors(trig_pins,echo_pins)
+
+def pull_trig_low(trig_pins):
+    for i in trig_pins:
+            GPIO.output(i, False)                 #Set TRIG as LOW
+            time.sleep(0.0002)  
+
+
+def pull_trig_high(trig_pins):
+    for i in trig_pins:
+            GPIO.output(i, True)                 #Set TRIG as LOW
+            time.sleep(0.0002)  
+
+
+
+def pull_echo_low(echo_pins):
+    for i in echo_pins:
+            GPIO.output(i, False)                 #Set TRIG as LOW
+            time.sleep(0.0002)  
+
+def pull_echo_high(echo_pins):
+    for i in echo_pins:
+            GPIO.output(i, True)                 #Set TRIG as LOW
+            time.sleep(0.0002)  
+
+def get_gpio_status(echo_pins):
+    for i in echo_pins:
+        if GPIO.input(i)==0:
+            return 0
+        else :
+            return 1
+
+
+while True:
+    time.sleep(1)
+    pull_trig_low(trig_pins)
+    #GPIO.output(SENSOR1_TRIG, False)                 #Set TRIG as LOW
+    print("Waiting For Sensors To Settle")
+    time.sleep(0.0002)  
+                              #Delay of 2 seconds
+    pull_trig_high(trig_pins)
+    #GPIO.output(SENSOR1_TRIG, True)                  #Set TRIG as HIGH
+    time.sleep(0.00001)                      #Delay of 0.00001 seconds
+    pull_trig_low(trig_pins)
+    #GPIO.output(SENSOR1_TRIG, False)                 #Set TRIG as LOW
+
+    while get_gpio_status(echo_pins)==0:
+        pulse_start = time.time()              #Time of the last  LOW pulse
+
+    while get_gpio_status(echo_pins)==1:               #Check whether Echo is HIGH
+        pulse_end = time.time()                 #Time of the last HIGH pulse 
+        pulse_duration = pulse_end - pulse_start  #pulse duration to a variable
+        distance = pulse_duration * 17150        #Calculate distance
+        distance = round(distance, 2)            #Round to two decimal points
+    if distance > 20 and distance < 400:     #Is distance within range
+        print("Distance:",distance - 0.5,"cm" ) #Distance with calibration
+        payload = {'sensor_id': '044ea4f4-fd82-482c-97d1-7c190c70cbd7', 'value': distance}
+        r = requests.post(url, data=payload)
+        print(r.text)
+        time.sleep(0.0002) 
+
+        # send it to api 
+    else:
+        print("Out Of Range")                  #display out of range
+
+
+
 
 
 
--- src/test5.py
+++ src/test5.py
@@ -1,7 +1,23 @@
 import requests
 
-payload = {'sensor_id': 'ead37d60-9cf4-11e9-a2a3-2a2ae2dbcce4', 'value': '500'}
+distance = 500
+
+payload = {'sensor_id': '044ea4f4-fd82-482c-97d1-7c190c70cbd7', 'value': '500'}
 
 r = requests.post("http://ultrapisensor.caramacs.com/api/sensor_data", data=payload)
 
-print(r.status_code)
+print(r.text)
+
+
+#sensor_data = { distance1 : 500 , distance2 : 250 , distance3:235, distance4:34, distance5: 45}
+
+"""
+
+def post_data(sensor_data):
+    for i in sensor_data:
+        payload = {'sensor_id': '044ea4f4-fd82-482c-97d1-7c190c70cbd7', 'value': i}
+        r = requests.post("http://ultrapisensor.caramacs.com/api/sensor_data", data=payload)
+        print(r.text)
+
+"""
+
